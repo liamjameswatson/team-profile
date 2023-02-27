1254ea746f0beb33fe4d42f71a795d894ae1cf2a
Liam <liamjwatson@aol.com>
2023-02-27 17:24:12 +0000

    asks for manager information in inquirer prompt, and console.logs the answers

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -10,6 +12,46 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+// Ask prompt questions for the Manager input
+const questionManagger = [
+  {
+    type: "input",
+    name: "managerName",
+    message: "Enter Manager Name:",
+  },
+  {
+    type: "input",
+    name: "managerId",
+    message: "Enter Manager Id:",
+  },
+  {
+    type: "input",
+    name: "managerEmail",
+    message: "Enter Email Address:",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "Enter Office Number",
+  },
+];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// function to run the prompt, get answers back, and store in a manager instance of the manager class.
+function init() {
+  inquirer.prompt(questionManagger).then((answers) => {
+    const manager = new Manager(
+      answers.managerName,
+      answers.managerId,
+      answers.managerEmail,
+      answers.officeNumber
+    );
+
+    // Display a success message and return to the main menu
+    console.log(`Success. Added ${manager.getName()} to database.`);
+    console.log(answers);
+  });
+}
+
+// calls the prompt function
+init();
 
